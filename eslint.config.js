89ddd691779dc9434343efa8bb2a159415c820b0
satish <satishatgmu@gmail.com>
2025-06-20 10:24:04 -0400

    fix linting issue

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import eslintPluginTs from '@typescript-eslint/eslint-plugin';
+import parserTs from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': eslintPluginTs,
+    },
+    rules: {
+      'no-console': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+    },
+  },
+];
